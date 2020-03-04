72f429f9b7fcab9e44aac6e04c0bad15936b0710
Cameron Short <short_c@lynchburg.edu>
2020-03-03 21:08:55 -0500

    Added command line arguments
    Also included some handling for directories that may have been created

--- Javacv_Testing/src/main/java/Ripper.java
+++ Javacv_Testing/src/main/java/Ripper.java
@@ -13,6 +13,17 @@ public class Ripper
 {
 	public static void main(String[] args) 
 	{
+		//Checking to ensure proper usage
+		checkProperArgs(args);
+		
+		//Creating any necessary directories
+		File dir = new File(args[1]);
+		if (!dir.exists()) dir.mkdirs(); 	
+		
+		dir = new File(args[2]); 
+		if (!dir.exists()) dir.mkdirs();
+		
+		
 		//Instantiating the structure that converts the grabbed frames to images that can be stored 
 		Java2DFrameConverter converter = new Java2DFrameConverter();
 		
@@ -23,27 +34,28 @@ public class Ripper
 		//The Buffered Image that will be written to disk 
 		BufferedImage im = null; 
 		
-		try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(new File("10secTimer.mp4"));
-				FFmpegFrameGrabber audioGrabber = new FFmpegFrameGrabber(new File("10secTimer.mp4")))
+		try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(new File(args[0]));
+				FFmpegFrameGrabber audioGrabber = new FFmpegFrameGrabber(new File(args[0])))
 		{
 			//Starting the grabber
 			frameGrabber.start();
 			audioGrabber.start(); 
 			
 			//FFmpegFrameRecorder will record the images back into video 
-			FFmpegFrameRecorder rec = new FFmpegFrameRecorder("VideoBrokenSounds\\outputAudio.mp3", audioGrabber.getAudioChannels()); 
+			FFmpegFrameRecorder rec = new FFmpegFrameRecorder(args[2]+"\\outputAudio.mp3", audioGrabber.getAudioChannels()); 
 			rec.setFormat("mp3");
 			rec.setAudioCodec(avcodec.AV_CODEC_ID_MP3);
 			rec.start();
 			
 			//Saving the video frames
-			int frameCounter = 0; 
+			int frameCounter = 0;
+			//frameGrabber.setTimestamp(10100000L);
 			while((vidFrame = frameGrabber.grab()) != null
 				|| (audioFrame = audioGrabber.grabSamples()) != null)
 			{
 				im = converter.convert(vidFrame); 
 				if(im != null)
-					ImageIO.write(im, "png", new File("VideoBrokenFrames\\frame"+(frameCounter++)+".png"));
+					ImageIO.write(im, "png", new File(args[1]+"\\frame"+(frameCounter++)+".png"));
 				
 				rec.recordSamples(audioFrame.samples);
 			}
@@ -59,4 +71,12 @@ public class Ripper
 		}
 	}
 
+	private static void checkProperArgs(String[] args)
+	{
+		if (args.length != 3)
+		{
+			System.err.println("Correct Usage: Ripper <file_to_be_ripped> <dir_to_output_frames> <dir_to_output_audio>"); 
+			System.exit(-1);
+		}
+	}
 }
