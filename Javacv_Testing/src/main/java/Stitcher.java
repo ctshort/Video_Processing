1e002d1998c0e191426b126ad1da1b97d21829f5
Cameron Short <short_c@lynchburg.edu>
2020-03-05 21:04:09 -0500

    Bug Fixed???
    Tested another video and code seemingly works fine

--- Javacv_Testing/src/main/java/Stitcher.java
+++ Javacv_Testing/src/main/java/Stitcher.java
@@ -13,68 +13,58 @@ public class Stitcher
 	public static void main(String[] args) 
 	{
 		//Ensuring proper usage of command-line args
-		//checkProperArgs(args);
+		checkProperArgs(args);
 				
 		//Checking if directory exists
-		//File dir = new File(args[1]);
-		File dir = new File("VideoStitchedVids");
+		File dir = new File(args[1]);
 		if (!dir.exists())					//If the directory doesn't exist 
 			dir.mkdirs(); 					//Create it and any other parent directories named in the command-line argument
 
 		//Getting the files in the VideoBrokenVids directory
-		//File vids = new File(args[1]); 
-		File vids = new File("VideoBrokenVids"); 
+		File vids = new File(args[0]); 
 		File[] vidDir = vids.listFiles();
 		
 		try
 		{
 			//Initializing some objects
-			Frame vidFrame = new Frame(); 
-			//Frame audioFrame = new Frame(); 
-			
-			FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(new File("VideoBrokenVids\\subVid0.mp4"));
-			//FFmpegFrameGrabber audioGrabber = new FFmpegFrameGrabber(new File("VideoBrokenVids\\subVid0.mp4"));
+			Frame vidFrame = new Frame(); 			
+			FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(new File(args[0]+"\\subVid0.mp4"));
 
-			//Starting the grabbers
+			//Starting the grabber
 			frameGrabber.start();
-			//audioGrabber.start(); 
 			
 			//Setting up recorder
-			//FFmpegFrameRecorder rec = new FFmpegFrameRecorder(args[2]+"stitchedVid.mp4", grabber.getImageWidth(), grabber.getImageHeight());
-			FFmpegFrameRecorder rec = new FFmpegFrameRecorder("VideoStitchedVids\\stitchedVid.mp4", frameGrabber.getImageWidth(), frameGrabber.getImageHeight(), 2);
+			FFmpegFrameRecorder rec = new FFmpegFrameRecorder(args[1]+"\\stitchedVid.mp4", frameGrabber.getImageWidth(), frameGrabber.getImageHeight(), 2);
 			setRecSettings(rec, frameGrabber);
 			rec.start(); 
 			
-			//Recording into new 
+			//Recording into new file
 			for(int i = 0; i < vidDir.length; ++i)
 			{
-				//while((vidFrame = frameGrabber.grab()) != null
-				//		|| (audioFrame = audioGrabber.grabSamples()) != null)
-				while((vidFrame = frameGrabber.grab()) != null)
+				while((vidFrame = frameGrabber.grabFrame()) != null)
 				{
-					rec.record(vidFrame); 
+					//rec.record(vidFrame); 
 					//rec.recordSamples(audioFrame.samples);
+					if (vidFrame.imageHeight != 0 && vidFrame.imageWidth != 0)
+						rec.record(vidFrame);
+					else if (vidFrame.samples != null)
+						rec.recordSamples(vidFrame.samples);
 				}
 
 				frameGrabber.stop(); frameGrabber.close(); 
-				//audioGrabber.stop(); audioGrabber.close();
 				if (i < vidDir.length-1) 
 				{
-					frameGrabber = new FFmpegFrameGrabber(new File("VideoBrokenVids\\subVid"+(i+1)+".mp4"));
-					//audioGrabber = new FFmpegFrameGrabber(new File("VideoBrokenVids\\subVid"+(i+1)+".mp4"));
+					frameGrabber = new FFmpegFrameGrabber(new File(args[0]+"\\subVid"+(i+1)+".mp4"));
 				}
 				else 
 				{
-					frameGrabber = new FFmpegFrameGrabber(new File("VideoBrokenVids\\subVid"+(i)+".mp4"));
-					//audioGrabber = new FFmpegFrameGrabber(new File("VideoBrokenVids\\subVid"+(i)+".mp4"));
+					frameGrabber = new FFmpegFrameGrabber(new File(args[0]+"\\subVid"+(i)+".mp4"));
 				}
 				frameGrabber.start();
-				//audioGrabber.start();
 			}
 			
 			//Closing resources
 			frameGrabber.stop(); frameGrabber.close(); 
-			//audioGrabber.stop(); audioGrabber.close();
 			rec.stop(); rec.close(); 
 		}
 		catch (Exception e)
@@ -98,6 +88,7 @@ public class Stitcher
 		rec.setFormat("mp4");
 		rec.setFrameRate(30);
 		rec.setSampleRate(grabber.getSampleRate());
+		rec.setVideoBitrate(grabber.getVideoBitrate());
 		rec.setAudioQuality(0);
 		rec.setAudioBitrate(grabber.getAudioBitrate());
 		rec.setAudioChannels(2);
