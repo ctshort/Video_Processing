81ea11b3c9376ed3772f851496a493a3427a2300
Cameron Short <short_c@lynchburg.edu>
2020-04-07 17:24:44 -0400

    Cleaned up commented out code

--- Javacv_Testing/src/main/java/Ripper.java
+++ Javacv_Testing/src/main/java/Ripper.java
@@ -25,65 +25,8 @@ public class Ripper
 		dir = new File(args[2]); 
 		if (!dir.exists()) dir.mkdirs();
 		
-		
-		//Instantiating the structure that converts the grabbed frames to images that can be stored 
-		//Java2DFrameConverter converter = new Java2DFrameConverter();
-		
-		//Storing the grabbed frame
-		//Frame vidFrame = new Frame();
-		//Frame audioFrame = new Frame(); 
-		
-		//The Buffered Image that will be written to disk 
-		//BufferedImage im = null; 
-		
-		
-		
-		//Testing ripping audio only 
 		ripAudio(args[0], args[2]);
 		ripFrames(args[0], args[1]); 
-		
-		
-		
-		/*
-		try (FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(new File(args[0]));
-				FFmpegFrameGrabber audioGrabber = new FFmpegFrameGrabber(new File(args[0])))
-		{
-			//Starting the grabber
-			frameGrabber.start();
-			audioGrabber.start(); 
-			
-			//FFmpegFrameRecorder will record the images back into video 
-			FFmpegFrameRecorder rec = new FFmpegFrameRecorder(args[2]+"\\outputAudio.mp3", audioGrabber.getAudioChannels()); 
-			rec.setFormat("mp3");
-			rec.setAudioCodec(avcodec.AV_CODEC_ID_MP3);
-			rec.start();
-			
-			//Saving the video frames
-			int frameCounter = 0;
-			while((vidFrame = frameGrabber.grab()) != null
-				|| (audioFrame = audioGrabber.grabSamples()) != null)
-			{
-				im = converter.convert(vidFrame); 
-				if(im != null)
-					ImageIO.write(im, "png", new File(args[1]+"\\frame"+(frameCounter++)+".png"));
-				
-				if (rec.getTimestamp() <= audioGrabber.getTimestamp())
-				{
-						rec.setTimestamp(audioGrabber.getTimestamp());
-						rec.recordSamples(audioFrame.samples);
-				}
-			}
-						
-			frameGrabber.stop(); frameGrabber.close(); 
-			audioGrabber.stop(); audioGrabber.close(); 
-			rec.stop(); rec.close(); 
-		}
-		catch (Exception e)
-		{
-			System.out.println("FAILURE: "+e+"\n"); 
-			e.printStackTrace();
-		}
-		*/
 	}
 	private static void ripAudio(String ripFile, String outDir)
 	{
